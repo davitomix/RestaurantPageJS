683357a8388700a968b2652eccd68150f8a4e8a0
David Elì <davidelimg1@gmail.com>
2020-04-21 20:42:14 -0500

    Implemented About content injector

--- src/about-page.js
+++ src/about-page.js
@@ -4,10 +4,10 @@ import Injector from './content-injector';
 const AboutObject = (() => {
   const domObject = DomObj;
   const injector = Injector;
-  
+
   const set = () => {
     let divObj = domObject.div;
-    divObj = injector.injectContent(divObj);
+    divObj = injector.injectAbout(divObj);
     domObject.setContent(divObj);
   };
 
--- src/content-injector.js
+++ src/content-injector.js
@@ -2,17 +2,17 @@ const Injector = (() => {
   const injecting = () => {
     console.log('injecting content');
   };
-  const injectContent = (element) => {
+  const injectAbout = (element) => {
     element.id = 'about-container';
     const subtitle = document.createElement('h2');
     subtitle.innerText = 'The best food in town! Sopa tarasca, Corundas, Enchiladas morelianas o placeras, Morisqueta, Carnitas, Aporreadillo, Uchepos, Chongos zamoranos, Ate, Nieve de pasta and much more...';
     element.appendChild(subtitle);
     return [element];
   };
-  
+
   return {
     injecting,
-    injectContent
+    injectAbout
   };
 })();
 
